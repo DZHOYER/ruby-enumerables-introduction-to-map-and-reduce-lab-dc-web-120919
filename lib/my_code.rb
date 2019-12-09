e2d5b5b1c58ea045e7b0023d1c000e238dd15937
Dzhoy Zuckerman <dzhoyz@gmail.com>
2019-12-09 02:09:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
 # My Code here....
+def map_to_negativize(array)
+  
+end
+
+def map_to_no_change(array)
+
+end
+
+def map_to_double(array)
+
+end
+
+def map_to_square(array)
+  
+end
+
+def reduce_to_total(source_array,starting_point)
+
+end
+
+def reduce_to_all_true(source_array)
+
+end
+
+def reduce_to_any_true(source_array)
+
+end
